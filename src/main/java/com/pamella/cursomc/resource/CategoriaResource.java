be0421f5826feeb93cf38b77db3e3019de662ae1
Pamella <pamella.godoi@gmail.com>
2020-04-27 21:36:08 -0300

    testando primeira classe de dominio

--- src/main/java/com/pamella/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/pamella/cursomc/resource/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.pamella.cursomc.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pamella.cursomc.domain.Categoria;
 
 // diretiva do spring boot
 
@@ -10,11 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class CategoriaResource {
 
+	private static final int Categoria = 0;
+
 	//para ser funcao tem que associar com algum verbo do http
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está ok";
+	public List<Categoria> listar() {
+		
+		// criaçao de objeto
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		// lista de categoria
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
 
